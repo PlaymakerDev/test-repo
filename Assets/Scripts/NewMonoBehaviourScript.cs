0bab3d4dd0c9c87957d1a7e943f07bb0dee36012
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2025-05-10 15:55:36 +0700

    alternate comment

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // SAMPLE DESCRIPTION
+        // SAMPLE DESCRIPTION NOT A DESCRIPTION
     }
 
     // Update is called once per frame
